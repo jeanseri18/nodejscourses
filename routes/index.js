f8a7a2f06014cb843ed138fdc52fee2425d758a8
seri jean setone <jean.seri@uniskip.com>
2022-10-29 14:56:05 +0000

    creation du projet

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+/* GET home page. */
+router.get('/form', function(req, res, next) {
+  
+  res.render('create', { title: 'Express' });
+});
+
+app.post('/createuser', (req, res,next) => {
+  console.log(res.body, { title: 'Express' });
+})
+
+module.exports = router;
